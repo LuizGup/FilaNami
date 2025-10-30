545c715fc998dfa0cf1af41acb07eb43326281c6
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2025-10-30 19:03:07 -0300

    Add GerenciarSenhas page and Funcionario components
    Introduces the GerenciarSenhas page for managing passwords, along with CardSenha and NavbarFuncionario components. Updates routing in App.jsx to include the new page and refactors route structure. Removes default styles from index.css.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SelectService from './components/SelectService';
-
+import SelectService from "./components/SelectService";
+import GerenciarSenhas from "./pages/Funcionario/GerenciarSenhas";
 
 function App() {
-
   return (
     <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<h1>Página Inicial</h1>} />
-            <Route path="/login" element={<h1>Página de Login</h1>} />
-            <Route path="/sign-up" element={<h1>Página de Registro</h1>} />
-            <Route path="/user" element={<h1>Página do Funcionario</h1>} >
-              <Route path="/user/select-service" element={<SelectService />} />
-            </Route>
-            <Route path="/admin" element={<h1>Página do Admin</h1>} />
-            <Route path="*" element={<h1>404 - Página não encontrada</h1>} />
-          </Routes>
+      <Routes>
+        <Route path="/" element={<h1>Página Inicial</h1>} />
+        <Route path="/login" element={<h1>Página de Login</h1>} />
+        <Route path="/sign-up" element={<h1>Página de Registro</h1>} />
+        <Route path="/user" element={<h1>Página do Funcionario</h1>} />
+        <Route path="/user/gerencia" element={<GerenciarSenhas />} />
+        <Route path="/toten" element={<SelectService />} />
+        <Route path="/admin" element={<h1>Página do Admin</h1>} />
+        <Route path="*" element={<h1>404 - Página não encontrada</h1>} />
+      </Routes>
     </BrowserRouter>
   );
 }
 
-
-export default App
+export default App;
---
+++ frontend/src/components/FuncionarioComponents/NavbarFuncionario/index.jsx
@@ -0,0 +1,11 @@
+import "./index.css"
+
+function NavbarFuncionario() {
+  return (
+    <nav className="navbar-funcionario">
+      <h2>Navbar Funcionario</h2>
+    </nav>
+  )
+}
+
+export default NavbarFuncionario;
