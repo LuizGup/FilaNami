84c27fb2e83b85a1b7e387fd0494c6d2aee08c2a
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2025-10-30 18:27:51 -0300

    Add React Router and setup initial routes
    Integrated react-router-dom and react-hook-form dependencies. Refactored App.jsx to use BrowserRouter and defined initial routes for main pages, including placeholders for login, registration, user, and admin sections. Moved Bootstrap imports to main.jsx. Added empty files for GerenciarSenhas page structure.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SelectService from './components/SelectService';
 
 
 function App() {
 
   return (
-    <div className="App">
-      <SelectService />
-    </div>
+    <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<h1>Página Inicial</h1>} />
+            <Route path="/login" element={<h1>Página de Login</h1>} />
+            <Route path="/sign-up" element={<h1>Página de Registro</h1>} />
+            <Route path="/user" element={<h1>Página do Funcionario</h1>} >
+              <Route path="/user/select-service" element={<SelectService />} />
+            </Route>
+            <Route path="/admin" element={<h1>Página do Admin</h1>} />
+            <Route path="*" element={<h1>404 - Página não encontrada</h1>} />
+          </Routes>
+    </BrowserRouter>
   );
 }
 
