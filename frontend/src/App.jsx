3a08f7995784e7aaf62a6521be3c4b3c19feb695
Nelson Neto <111544659+Nelsolaa@users.noreply.github.com>
2025-11-16 20:28:11 -0300

    Historico senhas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import GerenciarSenhas from "./pages/Funcionario/GerenciarSenhas";
 import SelectSector from "./pages/Totem/EscolhaSetor/SelectSector";
 import Login from "./pages/Login/index";
 import ShowKey from "./pages/Totem/MostrarSenha/ShowKey";
+import HistoricoSenhas from "./pages/Admin/HistoricoSenhas";
 function App() {
   return (
     <BrowserRouter>
@@ -17,6 +18,7 @@ function App() {
         <Route path="/toten" element={<SelectService />} />
         <Route path="/sector" element={<SelectSector />} />
         <Route path="/admin" element={<h1>Página do Admin</h1>} />
+        <Route path="/admin/historicoSenhas" element={<HistoricoSenhas/>} />
         <Route path="*" element={<h1>404 - Página não encontrada</h1>} />
         <Route path="/showkey" element={<ShowKey />} />
       </Routes>
