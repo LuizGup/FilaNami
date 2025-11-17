1e80e7ddf79911e3591eefac47a76bafb4d2e8dc
JoaoPauloDel <joaopaulodelvecchio@gmail.com>
2025-11-17 07:48:59 -0300

    Downgrade React to v18 and update admin route
    Changed React and related dependencies from v19 to v18 in package.json and package-lock.json for compatibility. Updated /admin route in App.jsx to render HomePageAdmin component instead of a placeholder.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,20 @@ import SelectService from "./pages/Totem/EscolhaPrioridade/SelectService";
 import GerenciarSenhas from "./pages/Funcionario/GerenciarSenhas";
 import SelectSector from "./pages/Totem/EscolhaSetor/SelectSector";
 import Login from "./pages/Login/index";
+import HomePageAdmin from "./pages/HomePageAdmin";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<h1>Página Inicial</h1>} />
         <Route path="/login" element={<Login />} />
-        <Route path="/login" element={<h1>Página de Login</h1>} />
         <Route path="/sign-up" element={<h1>Página de Registro</h1>} />
         <Route path="/user" element={<h1>Página do Funcionario</h1>} />
         <Route path="/user/gerenciar" element={<GerenciarSenhas />} />
         <Route path="/toten" element={<SelectService />} />
         <Route path="/sector" element={<SelectSector />} />
-        <Route path="/admin" element={<h1>Página do Admin</h1>} />
+        <Route path="/admin" element={<HomePageAdmin />} /> 
         <Route path="*" element={<h1>404 - Página não encontrada</h1>} />
       </Routes>
     </BrowserRouter>
