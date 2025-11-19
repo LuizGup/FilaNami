cdfeb0a208655853add40fbb7a04bc8d3f7662eb
Luiz Carlos <lcarlos.mln@gmail.com>
2025-11-19 19:31:34 -0300

    Refactor userService to use async functions
    Replaced direct exports with explicit async service functions for user operations. Improved readability and added existence checks for update and delete actions.

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -1,8 +1,37 @@
-const {
-  findAllUsers,
-  findUserById,
+const userDao = require("../repositories/userDao");
+
+const getAllUsers = async () => {
+  return await userDao.findAllUsers();
+};
+
+const getUserById = async (id) => {
+  const user = await userDao.findUserById(id);
+  return user;
+};
+
+const createUser = async (name, email, password, userType) => {
+  return await userDao.createUser(name, email, password, userType);
+};
+
+const updateUser = async (id, dataToUpdate) => {
+  const userExists = await userDao.findUserById(id);
+  if (!userExists) return null;
+
+  return await userDao.updateUser(id, dataToUpdate);
+};
+
+const deleteUser = async (id) => {
+  const userExists = await userDao.findUserById(id);
+  if (!userExists) return false;
+
+  await userDao.deleteUser(id);
+  return true;
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
   createUser,
   updateUser,
   deleteUser,
-} = require("../repositories/userDao");
-
+};
