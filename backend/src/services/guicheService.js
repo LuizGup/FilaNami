36f6a681f544fdb84e82026be15ed01bacbdf1cc
Nelson Neto <111544659+Nelsolaa@users.noreply.github.com>
2025-11-22 22:53:12 -0300

    coisas leves

--- backend/src/services/guicheService.js
+++ backend/src/services/guicheService.js
@@ -1,8 +1,45 @@
 const {
-  findAllGuiches,
-  findGuicheById,
-  createGuiche,
+  selectAllGuiches,
+  selectGuicheById,
+  insertGuiche,
   updateGuiche,
   deleteGuiche,
 } = require("../repositories/guicheDao");
 
+const getAllGuiches = async () => {
+  return await selectAllGuiches();
+};
+
+const getGuicheById = async (id) => {
+  const guiche = await selectGuicheById(id);
+  if (!guiche) {
+    throw new Error("Guiche not found");
+  }
+  return guiche;
+};
+
+const createGuiche = async (data) => {
+  const { numeroGuiche, senha, idSetor } = data;
+
+  if (!numeroGuiche || !senha || !idSetor) {
+    throw new Error("All fields (numeroGuiche, senha, idSetor) are required");
+  }
+
+  return await insertGuiche(numeroGuiche, senha, idSetor);
+};
+
+const updateGuicheData = async (id, data) => {
+  return await updateGuiche(id, data);
+};
+
+const removeGuiche = async (id) => {
+  return await deleteGuiche(id);
+};
+
+module.exports = {
+  getAllGuiches,
+  getGuicheById,
+  createGuiche,
+  updateGuicheData,
+  removeGuiche,
+};
